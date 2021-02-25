537669cd6d55ac33648f005ef8451a7a684f05b4
carlos-ebm <carlos.brito.1996@gmail.com>
2021-02-25 09:01:35 -0300

    server version 1.1

--- controllers/admin.js
+++ controllers/admin.js
@@ -188,7 +188,7 @@ function uploadAvatar(req, res) {
         if (req.files) {
           console.log(req.files);
           let filePath = req.files.avatar.path;
-          let fileSplit = filePath.split("\\");
+          let fileSplit = filePath.split("/");
           let fileName = fileSplit[2];
 
           let extSplit = fileName.split(".");
